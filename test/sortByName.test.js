d913d1ce416a2e28af087b917a953f51d83ed31f
Дина Абрамова <nightfox87@mail.ru>
2022-12-05 12:16:12 +0300

    Initial commit

---
+++ test/sortByName.test.js
@@ -0,0 +1,26 @@
+const sorting = require("../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+});
+
+describe("Books names test suit", () => {
+  it("Books names should not be sorted if only one book", () => {
+    expect(sorting.sortByName(["Гарри Поттер", "Гарри Поттер"])).toEqual([
+      "Гарри Поттер",
+      "Гарри Поттер",
+    ]);
+  });
+});
